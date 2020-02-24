14687824c5e5c2a89deb98d120e429df642fc312
pateldevang <devangpatel92@gmail.com>
2020-02-24 12:27:52 +0530

    To MVC and basic setup added.

---
+++ Hackgrid/Helpers/constants.swift
@@ -0,0 +1,15 @@
+//
+//  constants.swift
+//  Hackgrid
+//
+//  Created by Devang Patel on 24/02/20.
+//  Copyright © 2020 ADG-VIT. All rights reserved.
+//
+
+import Foundation
+
+
+class constants {
+    public static let baseurl = "https://--------.herokuapp.com"
+    public static let apiKey = ""
+}
---
+++ Hackgrid/Networking/NetworkEngine.swift
@@ -0,0 +1,23 @@
+//
+//  NetworkEngine.swift
+//  Hackgrid
+//
+//  Created by Devang Patel on 24/02/20.
+//  Copyright © 2020 ADG-VIT. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+
+// MARK: - NetworkEngine Class
+
+class NetworkEngine {
+    
+    
+    // TODO: - Network Calls
+    
+    
+    
+}
