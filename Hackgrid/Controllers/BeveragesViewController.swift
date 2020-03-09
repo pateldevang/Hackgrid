1110ef901626c64fe386d4b1cd25bbe9f45920ff
aaryankotharii <aaryan.kothari@gmail.com>
2020-03-09 11:51:10 +0530

    beveragesVC added

---
+++ Hackgrid/Controllers/BeveragesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BeveragesViewController.swift
+//  Hackgrid
+//
+//  Created by Aaryan Kothari on 09/03/20.
+//  Copyright © 2020 ADG-VIT. All rights reserved.
+//
+
+import UIKit
+
+class BeveragesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
