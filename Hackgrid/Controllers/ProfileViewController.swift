d5b475913ac397246cb6281bbe7603f460aaf869
aaryankotharii <aaryan.kothari@gmail.com>
2020-03-26 20:28:35 +0530

    ProfileVC added

---
+++ Hackgrid/Controllers/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Hackgrid
+//
+//  Created by Aaryan Kothari on 26/03/20.
+//  Copyright © 2020 ADG-VIT. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
