38169d61dfda04080351f1adb5c59a3ea339cb8e
aaryankotharii <aaryan.kothari@gmail.com>
2020-03-27 01:51:02 +0530

    LoginVC added

---
+++ Hackgrid/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Hackgrid
+//
+//  Created by Aaryan Kothari on 27/03/20.
+//  Copyright © 2020 ADG-VIT. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
